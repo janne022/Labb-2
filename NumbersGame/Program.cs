dad7f098d4078b0212fc31492bcac45cbd185ad5
Johannes Flodin <jannis020609@gmail.com>
2025-09-08 14:12:53 +0200

    added comments

--- NumbersGame/Program.cs
+++ NumbersGame/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Runtime.CompilerServices;
-
+// Start the game
 bool result = startGame();
+// Print the result
 if (result)
 {
     Console.WriteLine("Wohoo! Du klarade det!");
@@ -12,6 +13,7 @@ else if (!result)
 
 bool startGame()
 {
+    // Generate a random number between 1 and 20
     Console.WriteLine("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök");
     Random random = new Random();
     int correctNumber = random.Next(1, 20);
@@ -21,8 +23,10 @@ bool startGame()
         Console.Write("Min gissning:");
         string userInput = Console.ReadLine() ?? "";
         int guess;
+        // Get user input and check if it's a number
         bool success = int.TryParse(userInput, out guess);
-        if (success && timesGuessed <= 5)
+        // Check if the guess is correct, too high, or too low
+        if (success)
         {
             if (guess.Equals(correctNumber))
             {
--- NumbersGame/obj/Debug/net8.0/NumbersGame.AssemblyInfo.cs
+++ NumbersGame/obj/Debug/net8.0/NumbersGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NumbersGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e51b5e1d66b06d8ce62c43b0551643641deb1af3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04e9896e4da0cf204c27489cf3ae21cced9c1045")]
 [assembly: System.Reflection.AssemblyProductAttribute("NumbersGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NumbersGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
