a6c77030d60b8542bb2e1d31d0205d80effb0b2b
Johannes Flodin <jannis020609@gmail.com>
2025-09-21 23:00:43 +0200

    added random responses and more space between code

--- NumbersGame/Program.cs
+++ NumbersGame/Program.cs
@@ -5,14 +5,14 @@ namespace NumbersGame
     {
         private static void Main(string[] args)
         {
-            // Start the game
-            bool result = startGame();
+            // Start the game, get bool for if user won or not
+            bool isUserWinner = startGame();
             // Print the result
-            if (result)
+            if (isUserWinner)
             {
                 Console.WriteLine("Wohoo! Du klarade det!");
             }
-            else if (!result)
+            else if (!isUserWinner)
             {
                 Console.WriteLine("Tyvärr, du lyckades inte gissa talet på fem försök!");
             }
@@ -21,13 +21,17 @@ namespace NumbersGame
             {
                 // Declare variables. numberRange uses LINQ to filter an array of ints to an array of strings
                 string[] numberRange = Enumerable.Range(2, 59).Select(n => n.ToString()).ToArray();
+                string[] responsesHigh = ["Tyvärr, du gissade för högt!", "Haha! Det var för högt!", "Bra gissat, men det var för högt"];
+                string[] responsesLow = ["Tyvärr, du gissade för lågt!", "Haha! Det var för lågt!", "Bra gissat, men det var för lågt"];
                 int timesGuessed = 0;
                 var random = new Random();
                 int correctNumber = random.Next(1, 21);
+
                 // Generate a random number that user decides
                 Console.WriteLine("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök");
                 // Get roundAmount via slider. Could probably be improved by having its own method for ints
                 int roundAmount = Menu.ReadSlider("Hur många tal vill du gissa på?", numberRange);
+
                 do
                 {
                     // Get int via user input
@@ -39,11 +43,11 @@ namespace NumbersGame
                     }
                     else if (guess < correctNumber)
                     {
-                        Console.WriteLine("Tyvärr, du gissade för lågt!");
+                        Console.WriteLine(responsesLow[random.Next(0, responsesLow.Length)]);
                     }
                     else if (guess > correctNumber)
                     {
-                        Console.WriteLine("Tyvärr, du gissade för högt!");
+                        Console.WriteLine(responsesHigh[random.Next(0, responsesHigh.Length)]);
                     }
                     timesGuessed++;
                 } while (timesGuessed < roundAmount);
