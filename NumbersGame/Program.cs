6ea2501f4d7a0653c40239312ab8e56668032281
Johannes Flodin <jannis020609@gmail.com>
2025-09-21 22:53:43 +0200

    added help menu for this labb

--- NumbersGame/Program.cs
+++ NumbersGame/Program.cs
@@ -1,51 +1,54 @@
 ﻿using System.Runtime.CompilerServices;
-// Start the game
-bool result = startGame();
-// Print the result
-if (result)
+namespace NumbersGame
 {
-    Console.WriteLine("Wohoo! Du klarade det!");
-}
-else if (!result)
-{
-    Console.WriteLine("Tyvärr, du lyckades inte gissa talet på fem försök!");
-}
-
-bool startGame()
-{
-    // Generate a random number between 1 and 20
-    Console.WriteLine("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök");
-    Random random = new Random();
-    int correctNumber = random.Next(1, 21);
-    int timesGuessed = 0;
-    do
+    internal class Program
     {
-        Console.Write("Min gissning:");
-        string userInput = Console.ReadLine() ?? "";
-        int guess;
-        // Get user input and check if it's a number
-        bool success = int.TryParse(userInput, out guess);
-        // Check if the guess is correct, too high, or too low
-        if (success)
+        private static void Main(string[] args)
         {
-            if (guess.Equals(correctNumber))
+            // Start the game
+            bool result = startGame();
+            // Print the result
+            if (result)
             {
-                return true;
+                Console.WriteLine("Wohoo! Du klarade det!");
             }
-            else if (guess < correctNumber)
+            else if (!result)
             {
-                Console.WriteLine("Tyvärr, du gissade för lågt!");
+                Console.WriteLine("Tyvärr, du lyckades inte gissa talet på fem försök!");
             }
-            else if (guess > correctNumber)
+
+            bool startGame()
             {
-                Console.WriteLine("Tyvärr, du gissade för högt!");
+                // Declare variables. numberRange uses LINQ to filter an array of ints to an array of strings
+                string[] numberRange = Enumerable.Range(2, 59).Select(n => n.ToString()).ToArray();
+                int timesGuessed = 0;
+                var random = new Random();
+                int correctNumber = random.Next(1, 21);
+                // Generate a random number that user decides
+                Console.WriteLine("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök");
+                // Get roundAmount via slider. Could probably be improved by having its own method for ints
+                int roundAmount = Menu.ReadSlider("Hur många tal vill du gissa på?", numberRange);
+                do
+                {
+                    // Get int via user input
+                    int guess = Menu.ReadInt("Skriv in numret du vill gissa på:");
+                    // Check if the guess is correct, too high, or too low
+                    if (guess.Equals(correctNumber))
+                    {
+                        return true;
+                    }
+                    else if (guess < correctNumber)
+                    {
+                        Console.WriteLine("Tyvärr, du gissade för lågt!");
+                    }
+                    else if (guess > correctNumber)
+                    {
+                        Console.WriteLine("Tyvärr, du gissade för högt!");
+                    }
+                    timesGuessed++;
+                } while (timesGuessed < roundAmount);
+                return false;
             }
-            timesGuessed++;
         }
-        else if (!success)
-        {
-            Console.WriteLine("Du måste ange ett nummer!");
-        }
-    } while (timesGuessed < 5);
-    return false;
+    }
 }
